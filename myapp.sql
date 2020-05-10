90fcdaa0ce2878aec52cb4f98a4cecf5a781c253
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-10 21:49:43 +0900

    [finish]データ型

--- myapp.sql
+++ myapp.sql
@@ -1,12 +1,31 @@
+/*
+number:
+-int
+-float
+-double
+-int unsigned
+
+string:
+-char(4) 商品コードとか
+-varchar(255) メアドなどの改変超文字列
+-text 長さ不明
+
+date/time;
+-date
+-time
+-datetime "2017-07-22 22:22:22"
+
+true/false
+-boolean -> tinyint(1) //内部的には管理方法がtinyint
+ true 1(空文字でもtrue) false 0
+*/
 drop table if exists users;
 -- usersテーブルがあったら毎回削除
 create table users
 (
-  id int
-  unsigned,
-  name varchar
-  (20),
-  score float
+  id int unsigned, -- +の整数
+  name varchar (20), -- 20byteまでの文字列
+  score float --浮動小数点数
 );
 
 
